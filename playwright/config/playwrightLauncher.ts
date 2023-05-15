c8b93b61ce60157825e406b3c88ad93b4209fc08
rconner <rconner@applause.com>
2023-05-15 10:12:17 -0500

    AUT-6371 Refactor playwright to switch between executions easier

---
+++ playwright/config/playwrightLauncher.ts
@@ -0,0 +1,43 @@
+import { Browser, chromium } from 'playwright';
+import { Capabilities } from './remoteCapabilities';
+import { buildBrowserStackUrl, buildDatatapUrl } from './remoteUrlBuilder';
+
+const DEFAULT_TIMEOUT = 60_000;
+
+/**
+ * Launches a local headless Chromium browser
+ *
+ * @param timeoutMs The maximum amount of time to wait for the connection to be established
+ * @returns The Browser Instance
+ */
+export function localHeadlessChrome(timeoutMs?: number): Promise<Browser> {
+    return chromium.launch({ headless: true, timeout: timeoutMs || DEFAULT_TIMEOUT });
+}
+
+/**
+ * Connects to a browserstack instance
+ *
+ * @param caps The capabilities to launch
+ * @param timeoutMs The maximum amount of time to wait for the connection to be established
+ * @returns The Browser Instance
+ */
+export function bsConnect(caps: Capabilities, timeoutMs?: number): Promise<Browser> {
+    return chromium.connect(
+        buildBrowserStackUrl(caps),
+        { timeout: timeoutMs || DEFAULT_TIMEOUT }
+    );
+}
+/**
+ * Connects to a browserstack instance
+ *
+ * @param caps The capabilities to launch
+ * @param apiKey The apiKey to use to connect to datatap
+ * @param timeoutMs The maximum amount of time to wait for the connection to be established
+ * @returns The Browser Instance
+ */
+export function datatapConnect(caps: Capabilities, apiKey?: string, timeoutMs?: number): Promise<Browser> {
+    return chromium.connect(
+        buildDatatapUrl(caps, apiKey || 'FILL_ME_IN'),
+        { timeout: timeoutMs || DEFAULT_TIMEOUT }
+    );
+}
---
+++ playwright/config/remoteCapabilities.ts
@@ -0,0 +1,50 @@
+import * as cp from 'child_process';
+
+const clientPlaywrightVersion = cp.execSync('npx playwright --version')
+    .toString()
+    .trim()
+    .split(' ')[1];
+const BROWSERSTACK_USERNAME = process.env.BROWSERSTACK_USERNAME || 'FILL_ME_IN';
+const BROWSERSTACK_ACCESS_KEY = process.env.BROWSERSTACK_ACCESS_KEY || 'FILL_ME_IN';
+
+const BROWSERSTACK_CAPABILITIES = {
+    'browserstack.username': BROWSERSTACK_USERNAME,
+    'browserstack.accessKey': BROWSERSTACK_ACCESS_KEY,
+    'client.playwrightVersion': clientPlaywrightVersion,
+};
+
+export interface BaseCapabilities {
+    browser: string,
+    os: string,
+    os_version: string;
+    name: string;
+    build: string;
+}
+
+export interface BrowserStackCapabilities {
+    'browserstack.username': string;
+    'browserstack.accessKey': string;
+    'client.playwrightVersion': string;
+}
+
+export interface ApplauseOptions {
+    apiKey: string;
+    providerUrl: string;
+}
+
+export interface DatatapCapabilities {
+    'applause:options': ApplauseOptions;
+}
+
+export type Capabilities = BaseCapabilities & BrowserStackCapabilities;
+
+export function chromiumCaps(name?: string, build?: string): Capabilities {
+    return {
+        browser: 'playwright-chromium',
+        os: 'osx',
+        os_version: 'big sur',
+        name: name || 'Playwright-mocha test on Chromium',
+        build: build || 'playwright-mocha-build-1',
+        ...BROWSERSTACK_CAPABILITIES,
+    };
+}
